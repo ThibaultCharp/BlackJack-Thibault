497ca7f249a3cdf34950a5925c406d480b96ed3c
ThibaultCharp <538679@student.fontys.nl>
2025-04-15 10:20:30 +0200

    Add project files.

---
+++ BlackJack Thibault/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace BlackJack_Thibault
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.labelYou1 = new System.Windows.Forms.Label();
+            this.labelYou2 = new System.Windows.Forms.Label();
+            this.labelYou3 = new System.Windows.Forms.Label();
+            this.labelYou4 = new System.Windows.Forms.Label();
+            this.labelYou5 = new System.Windows.Forms.Label();
+            this.labelYou6 = new System.Windows.Forms.Label();
+            this.labelYou7 = new System.Windows.Forms.Label();
+            this.labelPC7 = new System.Windows.Forms.Label();
+            this.labelPC6 = new System.Windows.Forms.Label();
+            this.labelPC5 = new System.Windows.Forms.Label();
+            this.labelPC4 = new System.Windows.Forms.Label();
+            this.labelPC3 = new System.Windows.Forms.Label();
+            this.labelPC2 = new System.Windows.Forms.Label();
+            this.labelPC1 = new System.Windows.Forms.Label();
+            this.Hit = new System.Windows.Forms.Button();
+            this.Stand = new System.Windows.Forms.Button();
+            this.TotalYou = new System.Windows.Forms.Label();
+            this.TotalPC = new System.Windows.Forms.Label();
+            this.reset = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Yellow;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(527, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(203, 113);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click to play";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Yellow;
+            this.textBox1.Location = new System.Drawing.Point(117, 81);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(209, 45);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "You";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Yellow;
+            this.textBox2.Location = new System.Drawing.Point(909, 81);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(209, 45);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "Dealer";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // labelYou1
+            // 
+            this.labelYou1.AutoSize = true;
+            this.labelYou1.Location = new System.Drawing.Point(117, 153);
+            this.labelYou1.Name = "labelYou1";
+            this.labelYou1.Size = new System.Drawing.Size(0, 25);
+            this.labelYou1.TabIndex = 3;
+            // 
+            // labelYou2
+            // 
+            this.labelYou2.AutoSize = true;
+            this.labelYou2.Location = new System.Drawing.Point(117, 220);
+            this.labelYou2.Name = "labelYou2";
+            this.labelYou2.Size = new System.Drawing.Size(0, 25);
+            this.labelYou2.TabIndex = 4;
+            // 
+            // labelYou3
+            // 
+            this.labelYou3.AutoSize = true;
+            this.labelYou3.Location = new System.Drawing.Point(117, 289);
+            this.labelYou3.Name = "labelYou3";
+            this.labelYou3.Size = new System.Drawing.Size(0, 25);
+            this.labelYou3.TabIndex = 5;
+            // 
+            // labelYou4
+            // 
+            this.labelYou4.AutoSize = true;
+            this.labelYou4.Location = new System.Drawing.Point(117, 344);
+            this.labelYou4.Name = "labelYou4";
+            this.labelYou4.Size = new System.Drawing.Size(0, 25);
+            this.labelYou4.TabIndex = 6;
+            // 
+            // labelYou5
+            // 
+            this.labelYou5.AutoSize = true;
+            this.labelYou5.Location = new System.Drawing.Point(117, 403);
+            this.labelYou5.Name = "labelYou5";
+            this.labelYou5.Size = new System.Drawing.Size(0, 25);
+            this.labelYou5.TabIndex = 7;
+            // 
+            // labelYou6
+            // 
+            this.labelYou6.AutoSize = true;
+            this.labelYou6.Location = new System.Drawing.Point(117, 458);
+            this.labelYou6.Name = "labelYou6";
+            this.labelYou6.Size = new System.Drawing.Size(0, 25);
+            this.labelYou6.TabIndex = 8;
+            // 
+            // labelYou7
+            // 
+            this.labelYou7.AutoSize = true;
+            this.labelYou7.Location = new System.Drawing.Point(117, 511);
+            this.labelYou7.Name = "labelYou7";
+            this.labelYou7.Size = new System.Drawing.Size(0, 25);
+            this.labelYou7.TabIndex = 9;
+            // 
+            // labelPC7
+            // 
+            this.labelPC7.AutoSize = true;
+            this.labelPC7.Location = new System.Drawing.Point(913, 511);
+            this.labelPC7.Name = "labelPC7";
+            this.labelPC7.Size = new System.Drawing.Size(0, 25);
+            this.labelPC7.TabIndex = 16;
+            // 
+            // labelPC6
+            // 
+            this.labelPC6.AutoSize = true;
+            this.labelPC6.Location = new System.Drawing.Point(913, 458);
+            this.labelPC6.Name = "labelPC6";
+            this.labelPC6.Size = new System.Drawing.Size(0, 25);
+            this.labelPC6.TabIndex = 15;
+            // 
+            // labelPC5
+            // 
+            this.labelPC5.AutoSize = true;
+            this.labelPC5.Location = new System.Drawing.Point(913, 403);
+            this.labelPC5.Name = "labelPC5";
+            this.labelPC5.Size = new System.Drawing.Size(0, 25);
+            this.labelPC5.TabIndex = 14;
+            // 
+            // labelPC4
+            // 
+            this.labelPC4.AutoSize = true;
+            this.labelPC4.Location = new System.Drawing.Point(913, 344);
+            this.labelPC4.Name = "labelPC4";
+            this.labelPC4.Size = new System.Drawing.Size(0, 25);
+            this.labelPC4.TabIndex = 13;
+            // 
+            // labelPC3
+            // 
+            this.labelPC3.AutoSize = true;
+            this.labelPC3.Location = new System.Drawing.Point(913, 289);
+            this.labelPC3.Name = "labelPC3";
+            this.labelPC3.Size = new System.Drawing.Size(0, 25);
+            this.labelPC3.TabIndex = 12;
+            // 
+            // labelPC2
+            // 
+            this.labelPC2.AutoSize = true;
+            this.labelPC2.Location = new System.Drawing.Point(913, 220);
+            this.labelPC2.Name = "labelPC2";
+            this.labelPC2.Size = new System.Drawing.Size(0, 25);
+            this.labelPC2.TabIndex = 11;
+            // 
+            // labelPC1
+            // 
+            this.labelPC1.AutoSize = true;
+            this.labelPC1.Location = new System.Drawing.Point(913, 153);
+            this.labelPC1.Name = "labelPC1";
+            this.labelPC1.Size = new System.Drawing.Size(0, 25);
+            this.labelPC1.TabIndex = 10;
+            // 
+            // Hit
+            // 
+            this.Hit.BackColor = System.Drawing.Color.Yellow;
+            this.Hit.Location = new System.Drawing.Point(567, 156);
+            this.Hit.Name = "Hit";
+            this.Hit.Size = new System.Drawing.Size(117, 61);
+            this.Hit.TabIndex = 17;
+            this.Hit.Text = "Hit";
+            this.Hit.UseVisualStyleBackColor = false;
+            this.Hit.Click += new System.EventHandler(this.Hit_Click);
+            // 
+            // Stand
+            // 
+            this.Stand.BackColor = System.Drawing.Color.Yellow;
+            this.Stand.Location = new System.Drawing.Point(567, 223);
+            this.Stand.Name = "Stand";
+            this.Stand.Size = new System.Drawing.Size(117, 61);
+            this.Stand.TabIndex = 18;
+            this.Stand.Text = "Stand";
+            this.Stand.UseVisualStyleBackColor = false;
+            this.Stand.Click += new System.EventHandler(this.Stand_Click);
+            // 
+            // TotalYou
+            // 
+            this.TotalYou.AutoSize = true;
+            this.TotalYou.Location = new System.Drawing.Point(33, 579);
+            this.TotalYou.Name = "TotalYou";
+            this.TotalYou.Size = new System.Drawing.Size(0, 25);
+            this.TotalYou.TabIndex = 19;
+            // 
+            // TotalPC
+            // 
+            this.TotalPC.AutoSize = true;
+            this.TotalPC.Location = new System.Drawing.Point(1098, 596);
+            this.TotalPC.Name = "TotalPC";
+            this.TotalPC.Size = new System.Drawing.Size(0, 25);
+            this.TotalPC.TabIndex = 20;
+            // 
+            // reset
+            // 
+            this.reset.BackColor = System.Drawing.Color.Yellow;
+            this.reset.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reset.Location = new System.Drawing.Point(527, 464);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(203, 113);
+            this.reset.TabIndex = 21;
+            this.reset.Text = "RESET";
+            this.reset.UseVisualStyleBackColor = false;
+            this.reset.Click += new System.EventHandler(this.reset_Click_1);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResult.Location = new System.Drawing.Point(494, 329);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 42);
+            this.labelResult.TabIndex = 22;
+            this.labelResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(1266, 640);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.TotalPC);
+            this.Controls.Add(this.TotalYou);
+            this.Controls.Add(this.Stand);
+            this.Controls.Add(this.Hit);
+            this.Controls.Add(this.labelPC7);
+            this.Controls.Add(this.labelPC6);
+            this.Controls.Add(this.labelPC5);
+            this.Controls.Add(this.labelPC4);
+            this.Controls.Add(this.labelPC3);
+            this.Controls.Add(this.labelPC2);
+            this.Controls.Add(this.labelPC1);
+            this.Controls.Add(this.labelYou7);
+            this.Controls.Add(this.labelYou6);
+            this.Controls.Add(this.labelYou5);
+            this.Controls.Add(this.labelYou4);
+            this.Controls.Add(this.labelYou3);
+            this.Controls.Add(this.labelYou2);
+            this.Controls.Add(this.labelYou1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Casino BlackJack";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label labelYou1;
+        private System.Windows.Forms.Label labelYou2;
+        private System.Windows.Forms.Label labelYou3;
+        private System.Windows.Forms.Label labelYou4;
+        private System.Windows.Forms.Label labelYou5;
+        private System.Windows.Forms.Label labelYou6;
+        private System.Windows.Forms.Label labelYou7;
+        private System.Windows.Forms.Label labelPC7;
+        private System.Windows.Forms.Label labelPC6;
+        private System.Windows.Forms.Label labelPC5;
+        private System.Windows.Forms.Label labelPC4;
+        private System.Windows.Forms.Label labelPC3;
+        private System.Windows.Forms.Label labelPC2;
+        private System.Windows.Forms.Label labelPC1;
+        private System.Windows.Forms.Button Hit;
+        private System.Windows.Forms.Button Stand;
+        private System.Windows.Forms.Label TotalYou;
+        private System.Windows.Forms.Label TotalPC;
+        private System.Windows.Forms.Button reset;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ BlackJack Thibault/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlackJack_Thibault
+{
+    public partial class Form1 : Form
+    {
+        Random cardGenerator = new Random();
+        int sumPlayerCards = 0;
+        int sumPCCards = 0;
+        int[] PlayerCards = new int[6];
+        int[] PCCards = new int[6];
+        
+        int countPlayer = 0;
+        int i = 0;
+        int j = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            i = 0;
+            sumPlayerCards = 0;
+
+            PlayerCards[0] = cardGenerator.Next(1, 11);
+            i++;
+            PlayerCards[1] = cardGenerator.Next(1, 11);
+            PCCards[0] = cardGenerator.Next(1, 11);
+
+            for (i = 0; i < PlayerCards.Length; i++)
+            {
+                sumPlayerCards += PlayerCards[i];
+            }
+
+            TotalYou.Text = (sumPlayerCards.ToString());
+
+            labelPC1.Text = PCCards[0].ToString();
+            labelYou1.Text = PlayerCards[0].ToString();
+            labelPC2.Text = ("Hidden");
+            labelYou2.Text = PlayerCards[1].ToString();
+        }
+
+        private void Hit_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            countPlayer++;
+            sumPlayerCards = 0;
+
+            if (countPlayer == 1)
+            {
+                PlayerCards[2] = cardGenerator.Next(1, 11);
+                labelYou3.Text = PlayerCards[2].ToString();
+            }
+
+            if (countPlayer == 2)
+            {
+                PlayerCards[3] = cardGenerator.Next(1, 11);
+                labelYou4.Text = PlayerCards[3].ToString();
+            }
+
+            if (countPlayer == 3)
+            {
+                PlayerCards[4] = cardGenerator.Next(1, 11);
+                labelYou5.Text = PlayerCards[4].ToString();
+            }
+
+            if (countPlayer == 4)
+            {
+                PlayerCards[5] = cardGenerator.Next(1, 11);
+                labelYou6.Text = PlayerCards[5].ToString();
+            }
+
+            if (countPlayer == 5)
+            {
+                PlayerCards[6] = cardGenerator.Next(1, 11);
+                labelYou7.Text = PlayerCards[6].ToString();
+            }
+
+            for (int i = 0; i < PlayerCards.Length; i++)
+            {
+                sumPlayerCards += PlayerCards[i];
+            }
+
+            TotalYou.Text = (sumPlayerCards.ToString());
+
+            if (sumPlayerCards > 21)
+            {
+                labelResult.Text = ("Busted");
+            }
+        }
+        private void Stand_Click(object sender, EventArgs e)
+        {
+            Hit.Enabled = false;
+            PCCards[1] = cardGenerator.Next(1, 11);
+            for (j = 0; j < PCCards.Length; j++)
+            {
+                sumPCCards += PCCards[j];
+            }
+
+            if(sumPCCards < 17)
+            {
+                PCCards[2] = cardGenerator.Next(1, 11);
+                sumPCCards = sumPCCards + PCCards[2];
+                if (sumPCCards < 17)
+                {
+                    PCCards[3] = cardGenerator.Next(1, 11);
+                    sumPCCards = sumPCCards + PCCards[3];
+                    if (sumPCCards < 17)
+                    {
+                        PCCards[4] = cardGenerator.Next(1, 11);
+                        sumPCCards = sumPCCards + PCCards[4];
+                        if (sumPCCards < 17)
+                        {
+                            PCCards[5] = cardGenerator.Next(1, 11);
+                            sumPCCards = sumPCCards + PCCards[5];
+                            if (sumPCCards < 17)
+                            {
+                                PCCards[6] = cardGenerator.Next(1, 11);
+                                sumPCCards = sumPCCards + PCCards[6];
+                                if (sumPCCards < 17)
+                                {
+                                    PCCards[7] = cardGenerator.Next(1, 11);
+                                    sumPCCards = sumPCCards + PCCards[7];
+                                }
+                                else if (sumPCCards <= 21)
+                                {
+                                    labelPC2.Text = PCCards[1].ToString();
+                                    labelPC3.Text = PCCards[2].ToString();
+                                    labelPC4.Text = PCCards[3].ToString();
+                                    labelPC5.Text = PCCards[4].ToString();
+                                    labelPC6.Text = PCCards[5].ToString();
+                                    labelPC7.Text = PCCards[6].ToString();
+                                    TotalPC.Text = (sumPCCards.ToString());
+                                    Result(sumPCCards, sumPlayerCards);
+                                }
+                            }
+                            else if (sumPCCards <= 21)
+                            {
+                                labelPC2.Text = PCCards[1].ToString();
+                                labelPC3.Text = PCCards[2].ToString();
+                                labelPC4.Text = PCCards[3].ToString();
+                                labelPC5.Text = PCCards[4].ToString();
+                                labelPC6.Text = PCCards[5].ToString();
+                                TotalPC.Text = (sumPCCards.ToString());
+                                Result(sumPCCards, sumPlayerCards);
+                            }
+                        }
+                        else if (sumPCCards <= 21)
+                        {
+                            labelPC2.Text = PCCards[1].ToString();
+                            labelPC3.Text = PCCards[2].ToString();
+                            labelPC4.Text = PCCards[3].ToString();
+                            labelPC5.Text = PCCards[4].ToString();
+                            TotalPC.Text = (sumPCCards.ToString());
+                            Result(sumPCCards, sumPlayerCards);
+                        }
+                    }
+                    else if (sumPCCards <= 21)
+                    {
+                        labelPC2.Text = PCCards[1].ToString();
+                        labelPC3.Text = PCCards[2].ToString();
+                        labelPC4.Text = PCCards[3].ToString();
+                        TotalPC.Text = (sumPCCards.ToString());
+                        Result(sumPCCards, sumPlayerCards);
+                    }
+                }
+                else if (sumPCCards <= 21)
+                {
+                    labelPC2.Text = PCCards[1].ToString();
+                    labelPC3.Text = PCCards[2].ToString();
+                    TotalPC.Text = (sumPCCards.ToString());
+                    Result(sumPCCards, sumPlayerCards);
+                }
+            }
+            else if (sumPCCards <= 21)
+            {
+                labelPC2.Text = PCCards[1].ToString();
+                TotalPC.Text = (sumPCCards.ToString());
+                Result(sumPCCards, sumPlayerCards);
+            }
+            if (sumPCCards > 21)
+            {
+                labelResult.Text = ("Dealer Busted, You Win");
+                labelPC2.Text = PCCards[1].ToString();
+                labelPC3.Text = PCCards[2].ToString();
+                labelPC4.Text = PCCards[3].ToString();
+                labelPC5.Text = PCCards[4].ToString();
+                labelPC6.Text = PCCards[5].ToString();
+                if (PCCards.Length > 6)
+                {
+                    labelPC7.Text = PCCards[6].ToString();
+                }
+            }
+        }
+        private void Result(int sumPCCards, int sumPlayerCards)
+        {
+            if (sumPCCards > sumPlayerCards)
+            {
+                labelResult.Text = ("You Lose");
+            }
+            else if (sumPCCards < sumPlayerCards)
+            {
+                labelResult.Text = ("You Win");
+            }
+            else
+            {
+                labelResult.Text = ("Draw");
+            }
+        }
+        private void reset_Click_1(object sender, EventArgs e)
+        {
+            labelPC1.Text = ("");
+            labelPC2.Text = ("");
+            labelPC3.Text = ("");
+            labelPC4.Text = ("");
+            labelPC5.Text = ("");
+            labelPC6.Text = ("");
+            labelPC7.Text = ("");
+            labelYou1.Text = ("");
+            labelYou2.Text = ("");
+            labelYou3.Text = ("");
+            labelYou4.Text = ("");
+            labelYou5.Text = ("");
+            labelYou6.Text = ("");
+            labelYou7.Text = ("");
+            TotalPC.Text = ("");
+            TotalYou.Text = ("");
+            PlayerCards = new int[6];
+            PCCards = new int[6];
+            countPlayer = 0;
+            sumPCCards = 0;
+            sumPlayerCards = 0;
+            Hit.Enabled = true;
+            button1.Enabled = true;
+            labelResult.Text = ("");
+        }
+    }
+}
